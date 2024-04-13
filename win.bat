eb02d7cf832fb36d9534d817bde58d9bd110c285
afosne <62524488+afosne@users.noreply.github.com>
2024-04-13 22:52:07 +0800

    更新 win.bat

--- win.bat
+++ win.bat
@@ -4,8 +4,8 @@ rem //设置变量
 set DNS1=141.11.75.124
 
 echo 当前可用操作有：  
-echo   1 设置以太网DNS代理
-echo   2 设置以太网DNS代理
+echo   1 更改以太网DNS
+echo   2 更改VLANDNS
 echo   3 恢复动态DNS
 echo   4 退出  
 echo   .
@@ -21,9 +21,12 @@ if %operate%==4 goto 4
 echo . 
 echo 正在设置本地DNS代理，请稍等...  
 echo 网络 = 以太网   
-set /p ethn=若不止一个以太网请输入以太网后面的数字
-echo DNS  = %DNS1%   
+set /p ethn=若不止一个以太网请输入以太网后面的数字，若没有则输入0
 echo .
+if %ethn%==0 
+netsh interface ipv4 set dns name=以太网 source=static addr=%DNS1% register=PRIMARY
+echo DNS  = %DNS1%   
+if %ethn%>=0
 netsh interface ipv4 set dns name=以太网%ethn% source=static addr=%DNS1% register=PRIMARY   
 echo 本地代理已设置！  
 pause  
@@ -43,6 +46,17 @@ goto 4
 :3  
 echo .
 echo 正在恢复动态DNS，请稍等...  
+echo 选择你更改dns的网络适配器：  
+echo   1 以太网DNS
+echo   2 VLANDNS
+set /p oper=  
+if %oper%==1 
+set /p ethn=若不止一个以太网请输入以太网后面的数字，若没有则输入0
+if p>=0
+set NAME=以太网%ethn%
+if %ethn%==0
+set NAME=以太网
+if %oper%==2 set NAME=VLAN
 echo 网络 = %NAME%
 echo .
 netsh interface ip set dns name=%NAME% dhcp
