331dcc9df8473046ac4160e2997eab0ffb68e439
afosne <62524488+afosne@users.noreply.github.com>
2024-04-21 00:56:30 +0800

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 指定包含主机列表的逗号拼接的字符串
 hostlist="$@"
-IFS=',' read -ra hosts <<< "$hostlist"
+IFS='192.131.142.22,1.65.197.41,154.3.39.162,45.78.21.247,103.177.248.249,101.36.109.26,149.129.94.159,8.218.63.116,1.65.218.124,46.20.109.51' read -ra hosts <<< "$hostlist"
 for host in "${hosts[@]}"
 do
   curl -s -o /dev/null -m 1 -H "Host:www.netflix.com" -w "$host\t%{http_code}\t%{time_total}\n" http://$host
