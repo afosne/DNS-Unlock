0d15bcfe1e7e3e398109364706698b0eb4e7c86b
afosne <62524488+afosne@users.noreply.github.com>
2024-04-23 16:39:43 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var myHeaders = new Headers();
+myHeaders.append("X-Auth-Key", "${cf-api}"); //你的cloudflare api密钥
+myHeaders.append("X-Auth-Email", "${cf-mail}"); //你的cloudflare 邮箱
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+   "action": "override",
+   "filters": [
+      "dns"
+   ],
+   "description": "Unblock websites like streaming media",
+   "enabled": true,
+   "name": "TW", //cf DNS 策略名称
+   "rule_settings": {
+      "override_ips": [
+         "${proxy-ip1}", //需要代理的ip地址 可以通过 https://dnsip.afosne.workers.dev/?hostname=netflix.com&country=us借口获得
+         "${proxy-ip2}"
+      ]
+   },
+   "traffic": "any(app.type.ids[*] in {14}) and any(app.type.ids[*] in {11}) and any(app.ids[*] in {534 626 548 549 554 550 585})"
+});
+
+var requestOptions = {
+   method: 'POST',
+   headers: myHeaders,
+   body: raw,
+   redirect: 'follow'
+};
+
+fetch("https://api.cloudflare.com/client/v4/accounts/${account_id}/gateway/rules", requestOptions)
+   .then(response => response.text())
+   .then(result => console.log(result))
+   .catch(error => console.log('error', error));
